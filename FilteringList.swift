451e5cb3e2cee28ca1bc8453e6da9063df5711ae
Krisda Siangchaew <krisda.siangchaew@gmail.com>
2021-06-17 22:09:45 +0700

    Add StringInterpolation2

--- FilteringList.swift
+++ FilteringList.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  FilteringList.swift
 //  FilteringList
 //
---
+++ StringInterpolation2/StringInterpolation2/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  StringInterpolation2
+//
+//  Created by Krisda on 17/6/2564 BE.
+//
+
+import Foundation
+
+print("Hello, World!")
+
